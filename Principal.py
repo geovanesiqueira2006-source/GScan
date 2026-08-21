c63c95c017f51a60330f77ed20e272758957f124
geovanesiqueira2006 <geovanesiqueira2006@gmail.com>
2026-08-21 19:54:31 -0300

    Criação dos ifs e else

---
+++ Principal.py
@@ -0,0 +1,9 @@
+print("Bem Vindo ao GScan!")
+repositorio = input("Digite o endereço do seu Repositório:")
+print(f"Repositório Informado:{repositorio}")
+
+if repositorio == "":
+    print("Nenhum repositório foi informado.")
+else:
+    print("Repositório Informado:") 
+
