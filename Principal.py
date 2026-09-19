b55f1424013e8bd24965e44fd0a256a351c670f5
GEOVANE SIQUEIRA SILVA <geovane.silva006@cs.cruzeirodosul.edu.br>
2026-09-18 21:47:28 -0300

    validação de repositório e análise inicial

--- Principal.py
+++ Principal.py
@@ -1,9 +1,36 @@
-print("Bem Vindo ao GScan!")
-repositorio = input("Digite o endereço do seu Repositório:")
-print(f"Repositório Informado:{repositorio}")
+print("Bem Vindo ao GScan")
+
+repositorio = input("Digite o endereço do seu Repositório: ")
+
+print(repositorio)
+print(repositorio.startswith("https://github.com/"))
 
 if repositorio == "":
-    print("Nenhum repositório foi informado.")
+    print("Nenhum repositório foi informado")
 else:
-    print("Repositório Informado:") 
+# Verifica se o endereço informado começa com o GitHub
+    if repositorio.startswith("https://github.com/"):
+        print("Repositório válido")
 
+# Divide o endereço do repositório em partes
+        partes = repositorio.split("/")
+
+        usuario = partes [3]
+        repositorio_nome = partes[4]
+
+        print(f"Usuario: {usuario}")
+        print(f"Repositório: {repositorio_nome}")
+
+        print("Iniciando análise...")
+
+# Verifica se existe uma possível informação sensível
+        codigo = "minha senha é 123456"
+
+        if "senha" in codigo:
+         print("Risco encontrado!")
+
+    else:
+        print("Endereço de repositório inválido")
+        
+
+        
